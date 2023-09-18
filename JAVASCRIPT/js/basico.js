c9078250fdb424f4968ada99374c4736a2f9c4e0
Javier <javier@eskura21.com>
2023-09-18 08:29:18 +0200

    Fin de básico

--- JAVASCRIPT/js/basico.js
+++ JAVASCRIPT/js/basico.js
@@ -40,153 +40,196 @@ function saludar(nombre) {
     console.log('Hola ' + nombre);
 }
 
-const mes = 9;
-let dias;
+function sentenciasControl() {
+    const mes = 9;
+    let dias;
 
-switch (+mes) {
-    case 2:
-        dias = 28;
-        break;
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-        dias = 30;
-        break;
-    default:
-        dias = 31;
+    switch (+mes) {
+        case 2:
+            dias = 28;
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            dias = 30;
+            break;
+        default:
+            dias = 31;
+    }
+
+    console.log(mes, dias);
+
+    let i = 1;
+    while (i <= 10) {
+        console.log(i);
+        i++;
+    }
+
+    for (let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
 }
 
-console.log(mes, dias);
+function arrays() {
+    const arr = [1, 2, 3, 4, 5];
 
-let i = 1;
-while (i <= 10) {
-    console.log(i);
-    i++;
+    arr[3] = 6;
+
+    arr[5] = 10;
+
+    arr[7] = 'asdfadsf';
+
+    arr[10] = 1234;
+
+    arr['hola'] = 'QUé tal';
+
+    arr.adios = 'Pues vale';
+
+    console.log(typeof (arr), arr);
+    console.log(arr.length);
+
+    for (let indice = 0; indice < arr.length; indice++) {
+        console.log(arr[indice]);
+    }
+
+    for (let clave in arr) {
+        console.log(clave, arr[clave]);
+    }
+
+    for (let valor of arr) {
+        console.log(valor);
+    }
+
+    arr.forEach((valor, indice) => console.log('forEach', valor, indice)); //cadaUno);
+
+    // function cadaUno(valor, indice) {
+    //     console.log('forEach', valor, indice);
+    // }
+
+    const coleccion = [];
+
+    coleccion.push(1);
+    coleccion.push(2);
+    coleccion.push(3);
+
+    console.log(coleccion);
+
+    const diccionario = [];
+
+    diccionario.hola = 'hello';
+    diccionario.perro = 'dog';
+
+    console.log(diccionario);
+    console.log(diccionario.hola);
+
+    console.log(Object.keys(diccionario).length);
 }
 
-for (let i = 1; i <= 10; i++) {
-    console.log(i);
-}
+function objetos() {
+    const objeto = {};
 
-const arr = [1, 2, 3, 4, 5];
+    console.log(typeof (objeto), objeto);
 
-arr[3] = 6;
+    objeto.propiedad = 'valor';
+    objeto['otra'] = 'otro valor';
 
-arr[5] = 10;
-
-arr[7] = 'asdfadsf';
-
-arr[10] = 1234;
-
-arr['hola'] = 'QUé tal';
-
-arr.adios = 'Pues vale';
-
-console.log(typeof (arr), arr);
-console.log(arr.length);
-
-for (let indice = 0; indice < arr.length; indice++) {
-    console.log(arr[indice]);
-}
-
-for (let clave in arr) {
-    console.log(clave, arr[clave]);
-}
-
-for (let valor of arr) {
-    console.log(valor);
-}
-
-arr.forEach((valor, indice) => console.log('forEach', valor, indice)); //cadaUno);
-
-// function cadaUno(valor, indice) {
-//     console.log('forEach', valor, indice);
-// }
-
-const coleccion = [];
-
-coleccion.push(1);
-coleccion.push(2);
-coleccion.push(3);
-
-console.log(coleccion);
-
-const diccionario = [];
-
-diccionario.hola = 'hello';
-diccionario.perro = 'dog';
-
-console.log(diccionario);
-console.log(diccionario.hola);
-
-console.log(Object.keys(diccionario).length);
-
-const objeto = {};
-
-console.log(typeof(objeto), objeto);
-
-objeto.propiedad = 'valor';
-objeto['otra'] = 'otro valor';
-
-objeto.metodo = function () {
-    console.log(`
+    objeto.metodo = function () {
+        console.log(`
 propiedad: ${this.propiedad}
 otra: ${this.otra}
 `);
-    console.log('\n' +
-        'propiedad: ' + this.propiedad + '\n' +
-        'otra: ' + this.otra + '\n'
-    );
-};
+        console.log('\n' +
+            'propiedad: ' + this.propiedad + '\n' +
+            'otra: ' + this.otra + '\n'
+        );
+    };
 
-console.log(typeof (objeto), objeto);
-objeto.metodo();
-
-function Persona(nombre, apellido) {
-    this.nombre = nombre || null;
-    this.apellido = apellido || null;
+    console.log(typeof (objeto), objeto);
+    objeto.metodo();
 }
 
-const javier = new Persona('Javier', 'Lete');
-const pepe = new Persona('Pepe', 'Pérez');
+function prototipos() {
+    function Persona(nombre, apellido) {
+        this.nombre = nombre || null;
+        this.apellido = apellido || null;
+    }
 
-console.log(javier);
-console.log(pepe);
+    const javier = new Persona('Javier', 'Lete');
+    const pepe = new Persona('Pepe', 'Pérez');
 
-const persona = new Persona();
+    console.log(javier);
+    console.log(pepe);
 
-console.log(!!persona.nombre);
+    const persona = new Persona();
 
-persona.nombre = 'asdfasd';
-persona.apellido = 'asdfasdf';
+    console.log(!!persona.nombre);
 
-console.log(!!persona.trabajo);
+    persona.nombre = 'asdfasd';
+    persona.apellido = 'asdfasdf';
 
-persona.trabajo = 'Albañil';
+    console.log(!!persona.trabajo);
 
-console.log(persona);
+    persona.trabajo = 'Albañil';
 
-console.log(!!persona.trabajo);
+    console.log(persona);
 
-console.log(javier.trabajo);
-console.log(!!javier.trabajo);
+    console.log(!!persona.trabajo);
 
-Persona.prototype.informacion = function () {
-    // console.log('Nombre', this.nombre);
-    // console.log('Apellido', this.apellido);
+    console.log(javier.trabajo);
+    console.log(!!javier.trabajo);
 
-    Object.keys(this).forEach(clave => console.log(clave, this[clave]));
+    Persona.prototype.informacion = function () {
+        // console.log('Nombre', this.nombre);
+        // console.log('Apellido', this.apellido);
+
+        Object.keys(this).forEach(clave => console.log(clave, this[clave]));
+    }
+
+    javier.informacion();
+    pepe.informacion();
+    persona.informacion();
+
+    console.log('Hola'.toUpperCase());
+
+    String.prototype.toUpperCase = function () { return this + ' NO ME DA LA GANA DE CONVERTIRLO' };
+    String.prototype.saludar = function () { return 'Hola ' + this };
+
+    console.log('Hola'.toUpperCase());
+    console.log('Javier'.saludar());
 }
 
-javier.informacion();
-pepe.informacion();
-persona.informacion();
+function clases() {
+    class Persona {
+        constructor(nombre, apellido) {
+            this.nombre = nombre || null;
+            this.apellido = apellido || null;
+        }
+        informacion() {
+            // console.log('Nombre', this.nombre);
+            // console.log('Apellido', this.apellido);
+            Object.keys(this).forEach(clave => console.log(clave, this[clave]));
+        }
+    }
 
-console.log('Hola'.toUpperCase());
+    const javier = new Persona('Javier', 'Lete');
+    const pepe = new Persona('Pepe', 'Pérez');
 
-String.prototype.toUpperCase = function() { return this + ' NO ME DA LA GANA DE CONVERTIRLO'};
-String.prototype.saludar = function() { return 'Hola ' + this };
+    console.log(javier);
+    console.log(pepe);
 
-console.log('Hola'.toUpperCase());
-console.log('Javier'.saludar());
+    const persona = new Persona();
+
+    console.log(!!persona.nombre);
+
+    persona.nombre = 'asdfasd';
+    persona.apellido = 'asdfasdf';
+
+    console.log(!!persona.trabajo);
+
+    persona.trabajo = 'Albañil';
+
+    console.log(persona);
+}
+
+clases();
