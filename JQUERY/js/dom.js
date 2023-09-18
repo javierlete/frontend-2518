9ec55f443d9913279fcdb28de2305e76b25a1bd6
Javier <javier@eskura21.com>
2023-09-18 10:27:32 +0200

    Ejemplo DOM con jQuery

--- JQUERY/js/dom.js
+++ JQUERY/js/dom.js
@@ -1,44 +1,28 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const h1s = document.querySelectorAll('h1');
-    h1s.forEach(h1 => {
-        h1.innerHTML = h1.innerHTML.toUpperCase();
-    })
+$(function() {
+    $('h1').each(function() {
+        $(this).html($(this).html().toUpperCase());
+    });
 
-    const inputNombre = document.getElementById('nombre');
-    const botonSaludar = document.getElementById('saludar');
-    const spanMensaje = document.getElementById('mensaje');
-    const form = document.querySelector('form');
-
-    botonSaludar.addEventListener('click', () => {
+    $('#saludar').on('click', function() {
         console.log('Se ha hecho click');
     });
 
-    form.addEventListener('submit', (e) => {
+    $('form').on('submit', function(e) {
         e.preventDefault();
 
-        document.querySelectorAll('span.error').forEach((error) => {
-            error.remove();
+        $('span.error').each(function() {
+            this.remove();
         });
         
-        if (form.checkValidity()) {
-            inputNombre.classList.remove('error');
-            spanMensaje.innerText = 'Hola ' + inputNombre.value;
+        if ($('form')[0].checkValidity()) {
+            $('#nombre').removeClass('error');
+            $('#mensaje').html('Hola ' + $('#nombre').val());
         } else {
-            inputNombre.classList.add('error');
+            $('#nombre').addClass('error');
 
-            inputNombre.focus();
+            $('#nombre')[0].focus();
 
-            const spanError = document.createElement('span');
-            
-            spanError.classList.add('error');
-            spanError.innerText = 'Este campo es obligatorio';
-
-            //form.appendChild(spanError);
-            inputNombre.insertAdjacentElement('afterend', spanError);
+            $('<span class="error">Este campo es obligatorio</span>').insertAfter($('#nombre'));
         }
     });
-
-    console.log(inputNombre);
-    console.log(botonSaludar);
-    console.log(spanMensaje);
 });
