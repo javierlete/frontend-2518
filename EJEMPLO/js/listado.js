bcd855e39fa0d546d93651ea61ff545aa85455bd
Javier <javier@eskura21.com>
2023-09-19 09:48:10 +0200

    Añadir

--- EJEMPLO/js/listado.js
+++ EJEMPLO/js/listado.js
@@ -8,19 +8,51 @@ window.addEventListener('DOMContentLoaded', async () => {
 
     console.log(productos);
 
-    productos.forEach(p => {
-        const tr = document.createElement('tr');
+    productos.forEach(producto => {
+        nuevaLinea(producto);
+    });
 
-        tr.innerHTML = `
-            <th>${p.id}</th>
-            <td>${p.nombre}</td>
-            <td>${p.descripcion}</td>
-            <td>${p.precio} €</td>
+    document.querySelector('form').addEventListener('submit', async e => {
+        e.preventDefault();
+
+        const nombre = document.querySelector('#nombre').value;
+        const descripcion = document.querySelector('#descripcion').value;
+        const precio = document.querySelector('#precio').value;
+
+        const producto = { nombre, descripcion, precio };
+
+        const respuesta = await fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(producto)
+        });
+
+        if(respuesta.ok) {
+            alert('Producto añadido');
+
+            const producto = await respuesta.json();
+
+            nuevaLinea(producto);
+        } else {
+            alert('No se ha podido añadir');
+        }
+    });
+});
+
+function nuevaLinea(producto) {
+    const tr = document.createElement('tr');
+
+    tr.innerHTML = `
+            <th>${producto.id}</th>
+            <td>${producto.nombre}</td>
+            <td>${producto.descripcion}</td>
+            <td>${producto.precio} €</td>
             <td>
                 <a href="#" class="btn btn-primary btn-sm">Editar</a>
                 <a href="#" class="btn btn-danger btn-sm">Borrar</a>
             </td>`;
-        
-        document.querySelector('tbody').appendChild(tr);
-    });
-});
+
+    document.querySelector('tbody').appendChild(tr);
+}
