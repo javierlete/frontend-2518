cb95fd025273a2e11d52a8b5ff5eec45813f7fcc
Javier <javier@eskura21.com>
2023-09-19 09:09:13 +0200

    Listado

---
+++ EJEMPLO/js/listado.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const URL = 'json/productos.json';
+
+window.addEventListener('DOMContentLoaded', async () => {
+    const respuesta = await fetch(URL);
+    const productos = await respuesta.json();
+
+    console.log(productos);
+
+    productos.forEach(p => {
+        const tr = document.createElement('tr');
+
+        tr.innerHTML = `
+            <th>${p.id}</th>
+            <td>${p.nombre}</td>
+            <td>${p.descripcion}</td>
+            <td>${p.precio} €</td>
+            <td>
+                <a href="#" class="btn btn-primary btn-sm">Editar</a>
+                <a href="#" class="btn btn-danger btn-sm">Borrar</a>
+            </td>`;
+        
+        document.querySelector('tbody').appendChild(tr);
+    });
+});
