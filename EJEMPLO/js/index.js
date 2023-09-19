710c3040ef5f5eb47878f08df774585dbaeed439
Javier <javier@eskura21.com>
2023-09-19 09:00:41 +0200

    Listado inicial de productos

---
+++ EJEMPLO/js/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const URL = 'json/productos.json';
+
+window.addEventListener('DOMContentLoaded', async () => {
+    const respuesta = await fetch(URL);
+    const productos = await respuesta.json();
+
+    console.log(productos);
+
+    productos.forEach(p => {
+        const card = document.createElement('div');
+        card.className = 'col';
+
+        card.innerHTML =
+            `<div class="card h-100">
+                <img src="https://picsum.photos/200/150?${p.id}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">${p.nombre}</h5>
+                    <p class="card-text">${p.descripcion}</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-body-secondary">${p.precio} €</small>
+                </div>
+            </div>`;
+        
+        document.querySelector('#productos').appendChild(card);
+    });
+});
